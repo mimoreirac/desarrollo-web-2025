ab85c8bf8d4eb4ad1463f610c1143363be02d610
Martin Moreira <172557112+mimoreirac@users.noreply.github.com>
2025-06-18 22:53:28 -0500

    18-jun MongoDB connection

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-let express = require("express");
-let app = express();
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const mongoUri = process.env.MONGODB_URI;
+const port = process.env.PORT || 4000;
+
+mongoose
+  .connect(mongoUri)
+  .then(() => console.log("Conectado a MongoDB"))
+  .catch((err) => console.error("Error de conexión en la base de datos", err));
+
 let tareasRoutes = require("./routes/tareas");
 
 app.use(express.json());
 app.use("/tareas", tareasRoutes);
 
-app.listen(4000, () => {
+app.listen(port, () => {
   console.log("Servidor corriendo en el puerto 4000");
 });
